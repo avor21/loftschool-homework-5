a9a03030a45a570b2da603a3dbc450801579209d
Alina Voronina <skorokhodova.a5@gmail.com>
2020-01-20 23:05:34 +0300

    Добавлена форма авторизации и логика работы с ней

--- src/Auth.js
+++ src/Auth.js
@@ -1,8 +1,46 @@
 import React, {Component} from 'react';
+import {authorizeUser} from "./AuthorizeApi";
 
 class Auth extends Component {
+  state = {
+    email: '',
+    password: '',
+    isAuthorized: null
+  };
+
+  inputHandler = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value })
+  };
+
   render() {
-    return <h2>Auth page</h2>;
+    const { email, password, isAuthorized } = this.state;
+    const hasError = isAuthorized !== null && !isAuthorized;
+    return (
+      <form>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input name="email"
+                 value={email}
+                 onChange={this.inputHandler} />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input name="password"
+                 type="password"
+                 value={password}
+                 onChange={this.inputHandler} />
+        </div>
+        { hasError && <p>Неверный Email и/или пароль</p> }
+        <button onClick={this.handleSubmit}>Submit</button>
+      </form>
+    );
+  }
+
+  handleSubmit = (event) => {
+    const { email, password } = this.state;
+    event.preventDefault();
+    this.setState({ isAuthorized: authorizeUser(email, password) });
   }
 }
 
